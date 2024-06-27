de6b144fe86b188956f9986c5f71fb7c136c98b1
Miguel Molina Jr. <molim134@gmail.com>
2024-06-27 14:32:23 -0400

    Make artificats are easier to read via pretty print and those that prefer bytecode have generated bytecode in the contracts

--- TokenVault/generateArtifacts.js
+++ TokenVault/generateArtifacts.js
@@ -9,11 +9,13 @@ const throwError = error => {
   }
 }
 
+const stringify = obj => JSON.stringify(obj, null, 4);
+
 try {
   const mintingArtifact = compileFile(new URL('TokenVault_Mint.cash', import.meta.url));
   const releaseArtifact = compileFile(new URL('TokenVault_Release.cash', import.meta.url));
-  writeFile('TokenVault/TokenVault_Mint.artifact.cash', JSON.stringify(mintingArtifact), throwError);
-  writeFile('TokenVault/TokenVault_Release.artifact.cash', JSON.stringify(releaseArtifact), throwError);
+  writeFile('TokenVault/TokenVault_Mint.artifact.cash', stringify(mintingArtifact), throwError);
+  writeFile('TokenVault/TokenVault_Release.artifact.cash', stringify(releaseArtifact), throwError);
   logger.info('Artifacts created and saved to file.');
 } catch (e) {
   logger.error('error: ' + e.toString());
