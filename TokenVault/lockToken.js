ce3f249ec6e36b9d1d1f0d76d84d90aa4054b6a1
Miguel Molina Jr. <molim134@gmail.com>
2024-08-07 22:22:07 -0400

    Update lockToken script to work w/ locking an existing token

--- TokenVault/lockToken.js
+++ TokenVault/lockToken.js
@@ -1,5 +1,4 @@
 import { ElectrumNetworkProvider, Contract } from 'cashscript';
-import { compileFile } from 'cashc';
 
 import { DustValue } from '../util/constants.js';
 import { prompt, promptInt, promptBool } from '../util/prompt.js';
@@ -12,41 +11,30 @@ import buildReleaseTokenContract from './buildReleaseTokenContract.js';
 
 import config from '../config.json' assert { type: 'json' };
 
-
 try {
   const network = config.Network;
   const networkProvider = new ElectrumNetworkProvider(network);
   const networkHeight = await networkProvider.getBlockHeight();
   logger.info(`network: ${network} height: ${networkHeight}`);
 
-  const { signatureTemplate, pubKeyHex: fundHex, address: fundAddress } = await getWallet();
+  const { signatureTemplate, address: fundAddress } = await getWallet();
 
-  const mintingContractJson = compileFile(new URL('TokenVault_Mint.cash', import.meta.url));
-  const mintingContract = new Contract(mintingContractJson, [fundHex], { addressType: config.Contract.AddressType, provider: networkProvider });
   const { releaseContract, halvingLength } = await buildReleaseTokenContract({ addressType: config.Contract.AddressType, provider: networkProvider });
 
-  const balance = await mintingContract.getBalance();
-  logger.debug('balance: ' + balance);
-  if (balance == 0n) {
-    throw Error('The contract does not contain a balance...send funds to ' + mintingContract.address);
-  }
+  const fundUtxos = await networkProvider.getUtxos(fundAddress);
+  logger.debug('fund UTXOs', fundUtxos);
 
-  const contractUtxos = await mintingContract.getUtxos();
-  if (contractUtxos.length == 0) {
-    throw Error('No UTXOs found for this contract...ensure a valid UTXO exists');
-  }
-
-  const InputMinimumSatoshis = 4000;
+  const InputMinimumSatoshis = 10000;
 
   const potentialInputs = [];
   let utxoIndex = 0;
   do {
-    const utxo = contractUtxos[utxoIndex];
-    if (utxo.satoshis >= InputMinimumSatoshis && utxo.token == undefined) {
+    const utxo = fundUtxos[utxoIndex];
+    if (utxo.satoshis >= InputMinimumSatoshis && utxo.token !== undefined && utxo.token.amount > 0 && utxo.token.nft === undefined) {
       potentialInputs.push(utxo);
     }
     ++utxoIndex;
-  } while (utxoIndex < contractUtxos.length);
+  } while (utxoIndex < fundUtxos.length);
 
   const selectInput = () => {
     let selectedInput;
@@ -54,7 +42,7 @@ try {
       if (potentialInputs.length == 1) {
         selectedInput = potentialInputs[0];
       } else if (potentialInputs.length > 1) {
-        const response = prompt(`Which input should be used:\n${potentialInputs.map((i, index) => `(${index}) Id: ${i.txid} VOut: ${i.vout} Satoshis: ${i.satoshis}`).join('\n')}`);
+        const response = prompt(`Which input should be used:\n${potentialInputs.map((i, index) => `(${index}) Id: ${i.txid} VOut: ${i.vout} Satoshis: ${i.satoshis} Token: ${i.token}`).join('\n')}`);
         const index = Number.parseInt(response);
         if (index != NaN) {
           selectedInput = matches[0];
@@ -67,11 +55,10 @@ try {
   const input = selectInput();
 
   if (!input) {
-    throw Error('No satisfactory UTXOs found for this contract...ensure a valid UTXO exists without any tokens');
+    throw Error('No satisfactory UTXOs found for this contract...ensure a valid UTXO exists with some tokens');
   }
 
-  const tokenAmount = promptInt('How many tokens to issue (96,934,680,000,000,000)? ', '96934680000000000');
-  const lockAmount = promptInt(`How many tokens to lock (76,934,680,000,000,000)? `, ' 76934680000000000');
+  const tokenAmount = input.token.amount;
   const blockHeight = promptInt('Start releasing tokens at block (0)? ', '0');
   const reward = promptInt('Initial reward (250,000,000,000)? ', '250000000000');
 
@@ -81,31 +68,23 @@ try {
     throw Error('User denied input values and script is aborting.');
   }
 
-  let changeAmount = tokenAmount - lockAmount;
-  let changeToken;
-  if (changeAmount > 0) {
-    changeToken = {
-      amount: changeAmount,
-      category: input.txid
-    };
-  }
-
   const buildTransaction = fee => {
     const changeValue = input.satoshis - (2n * DustValue) - fee;
     if (changeValue < DustValue) {
       throw Error('The UTXO found does not contain enough value, reclaim and then provide a new UTXO with a higher value.')
     }
-    return mintingContract.functions.mint(signatureTemplate, tokenAmount, lockAmount, blockHeight, reward)
-      .withoutChange()
+    const builder = new TransactionBuilder({ provider: networkProvider });
+
+    return builder.withoutChange()
       .withoutTokenChange()
-      .from(input)
-      .to(releaseContract.tokenAddress,
+      .addInput(input, signatureTemplate.unlockP2PKH())
+      .addOutput(releaseContract.tokenAddress,
         DustValue,
         {
-          amount: lockAmount,
-          category: input.txid
+          amount: tokenAmount,
+          category: input.token.category
         })
-      .to(releaseContract.tokenAddress,
+      .addOutput(releaseContract.tokenAddress,
         DustValue,
         {
           amount: 0n,
@@ -115,9 +94,7 @@ try {
             commitment: `${encodeBigIntToHexLE(blockHeight)}${encodeBigIntToHexLE(blockHeight)}${encodeBigIntToHexLE(reward)}`
           }
         })
-      .to(fundAddress,
-        changeValue,
-        changeToken);
+      .addOutput(fundAddress, changeValue);
   };
 
   let transaction = buildTransaction(DustValue * 2n);
